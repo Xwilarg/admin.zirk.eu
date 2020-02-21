2c8f3582c8bfc86467efdee546ca33e411dcbf11
Xwilarg <xwilarg@yahoo.fr>
2020-02-22 02:07:00 +0900

    Add ability to delete users

--- js/index.js
+++ js/index.js
@@ -8,6 +8,57 @@
             document.location.href = "login.html";
         });
 
+        function deleteUser(username) {
+            let postData = new URLSearchParams();
+            postData.append('token', token);
+            postData.append('username', username);
+            fetch("https://restarter.zirk.eu/userDelete", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: postData
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.text();
+                }
+                throw new Error("HTTP " + response.status);
+            })
+            .then(_ => {
+                updateUserInfos();
+            })
+            .catch(error => console.error("Error while deleting user: " + error));
+        }
+
+        function updateUserInfos() {
+            let postData = new URLSearchParams();
+            postData.append('token', token);
+            fetch("https://restarter.zirk.eu/userList", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: postData
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error("HTTP " + response.status);
+            })
+            .then(result => {
+                let str = "";
+                result.users.forEach(function(user) {
+                    str += user.username + ": " + user.permissions + (user.permissions !== -1 ? ' <button onclick="deleteUser(\'' + user.username + '\')">Delete</button>' : '') + "<br/>";
+                });
+                document.getElementById("userList").innerHTML = str;
+            })
+            .catch(error => document.getElementById("userList").innerHTML = error);
+        }
+
+        updateUserInfos();
+
         document.getElementById("createButton").addEventListener("click", function() {
             document.getElementById("createStatus").innerHTML = "Loading...";
             let postData = new URLSearchParams();
@@ -32,31 +83,5 @@
             })
             .catch(error => document.getElementById("createStatus").innerHTML = error);
         });
-
-        { /// Load users
-            let postData = new URLSearchParams();
-            postData.append('token', token);
-            fetch("https://restarter.zirk.eu/userList", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: postData
-            })
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error("HTTP " + response.status);
-            })
-            .then(result => {
-                let str = "";
-                result.users.forEach(function(user) {
-                    str += user.username + ": " + user.permissions + "<br/>";
-                });
-                document.getElementById("userList").innerHTML = str;
-            })
-            .catch(error => document.getElementById("userList").innerHTML = error);
-        }
     }
 }
